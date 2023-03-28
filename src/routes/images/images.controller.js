199624c6e9d809d91f5af74adb97f5810066fd5b
hammadtariqatdatics <hammad.tariq@datics.ai>
2023-03-28 12:38:01 +0500

    image-modal-added

--- src/routes/images/images.controller.js
+++ src/routes/images/images.controller.js
@@ -5,11 +5,20 @@ const router = express.Router();
 const db = require("../../../db/models");
 const { Images } = db;
 
+// Use storage engine of multer to store the files
 
-const diskStorage = multer.diskStorage()
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "pictures");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
 // Create multer object
 const imageUpload = multer({
-  dest: "images"
+  storage: storage,
 });
 
 // Image Get Routes
@@ -20,7 +29,7 @@ router.get("/:filename", async (req, res) => {
       fileName: filename,
     },
   });
-//   console.log(images[0], "hello");
+  //   console.log(images[0], "hello");
   if (images[0]) {
     const dirName = path.resolve();
     const fullFilePath = path.join(dirName, images[0].filePath);
@@ -32,7 +41,26 @@ router.get("/:filename", async (req, res) => {
 
 // Image Upload Routes
 router.post("/upload", imageUpload.single("image"), async (req, res) => {
-  //   console.log(req.file, "hello");
+    // console.log(req.file, "hello");
+  const { filename, mimetype, size } = req.file;
+  const filePath = req.file.path;
+
+  const imagesData = await Images.create({
+    fileName: filename,
+    filePath: filePath,
+    type: mimetype,
+    size: size,
+  });
+  if (imagesData) {
+    res.status(200).send({ message: "File Uploaded", data: imagesData });
+  } else {
+    res.status(400).send({ message: "File Uploaded Failed..." });
+  }
+});
+
+// Image Upload Routes
+router.post("/bulk", imageUpload.array("images", 4), async (req, res) => {
+  console.log(req.file, "hello");
   const { filename, mimetype, size } = req.file;
   const filePath = req.file.path;
 
